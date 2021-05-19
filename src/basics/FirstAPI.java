d29c1934cd9ca46e7e003a8bf35c7b2b244119f6
Tajinder_Singh_Dhoot <singhtazinder047@gmail.com>
2021-05-19 02:28:03 -0400

    use of POST, PUT and GET methods on API

--- src/basics/FirstAPI.java
+++ src/basics/FirstAPI.java
@@ -1,5 +1,56 @@
 package basics;
 
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import files.Payload;
+
 public class FirstAPI {
 
+	public static void main(String[] args) {
+		
+		//given - all input fields
+		//when - http method, submit API resources
+		//then - validate the response
+		
+		RestAssured.baseURI = "https://rahulshettyacademy.com";
+		
+		String response = given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+		.body(Payload.AddPlace())
+		.when().post("maps/api/place/add/json")
+		.then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP"))
+		.header("Server", "Apache/2.4.18 (Ubuntu)").extract().response().asString();
+		
+		System.out.println(response);
+		
+		JsonPath js = new JsonPath(response); // takes input as String and converts it into JSOn format
+		String placeId = js.getString("place_id");
+		
+		System.out.println(placeId);
+		
+		//update the place 
+		
+		String modifiedAddress = "70 Summer walk, USA";
+		
+		given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+		.body("{\r\n"
+				+ "\"place_id\":\""+placeId+"\",\r\n"
+				+ "\"address\":\""+modifiedAddress+"\",\r\n"
+				+ "\"key\":\"qaclick123\"\r\n"
+				+ "}")
+		.when().put("maps/api/place/update/json")
+		.then().log().all().assertThat().statusCode(200);
+		
+		// to get place 
+		
+		given().log().all().queryParam("key", "qaclick123").queryParam("place_id", placeId)
+		.when().get("maps/api/place/get/json")
+		.then().log().all().assertThat().statusCode(200).body("address", equalTo("70 Summer walk, USA"));
+		
+		
+		
+		}
 }
